f0b0bf4fb9c5c8fe3a24653b4f7bcac9d96666f2
ksjogo <jogo@kronberger-spiele.de>
2015-07-21 16:16:48 +0200

    Added Lookup class features

--- src/dkd/RdfaExporter.java
+++ src/dkd/RdfaExporter.java
@@ -22,68 +22,72 @@ import java.util.Set;
 @CreoleResource(name = "RDFa Lite Exporter", comment = "Exports Annotations as RDFa Lite", tool = true, autoinstances = @AutoInstance, icon="")
 public class RdfaExporter extends DocumentExporter
 {
-    public RdfaExporter()
-    {
-        super("HTML5 RDFa","html","text/html");
-    }
-
-    private String extractClass(String input)
-    {
-        if (input.contains("|"))
-            input = input.split("\\|")[0];
-
-        if (input.contains(":"))
-            input = input.split(":")[1];
-
-        return input;
-    }
-
-    @Override
-    public void export(Document document, OutputStream out, FeatureMap options) throws IOException
-    {
-        PrintStream pout = new PrintStream(out);
-        AnnotationSet outputAS = document.getAnnotations(GateConstants.ORIGINAL_MARKUPS_ANNOT_SET_NAME);
-        AnnotationSet mentions = document.getAnnotations().get("Mention");
-
-        //store created annotations so we can remove them later, not relevant in wasp
-        //but quite relevant in gate.app usecase
-        Set<Integer> created = new HashSet<Integer>();
-
-        try
+        public RdfaExporter()
         {
-            for (Annotation annotation : mentions)
-            {
-                FeatureMap params = Factory.newFeatureMap();
-                params.put("vocab", "http://dbpedia.org/ontology/");
-                params.put("resource", annotation.getFeatures().get("inst").toString());
-
-                params.put("typeof",extractClass(annotation.getFeatures().get("dbpSpecificClass").toString()));
-                params.put("base",extractClass(annotation.getFeatures().get("dbpInterestingClass").toString()));
-
-                Long start = annotation.getStartNode().getOffset();
-                Long end = annotation.getEndNode().getOffset();
-
-                created.add(outputAS.add(start, end, "span", params));
-            }
-
-            String result = document.toXml(null, false);
-            //the gate html document implementation will have added this stuff to the ORIGINAL_MARKUPS
-            //we do not want these in the RTE or other endpoints
-            if (options.containsKey("stripOuter") && ((String)options.get("stripOuter")).equals("true"))
-                {
-                    result = result.replace("<html><head></head><body>","");
-                    result = result.replace("</body></html>","");
-                }
-            pout.println(result);
+                super("HTML5 RDFa","html","text/html");
         }
-        catch(Exception e)
-            {
-                throw new IOException(e);
-            }
-        finally
-            {
-                for (Integer id : created)
-                    outputAS.remove(outputAS.get(id));
-            }
-    }
-    }
+
+        private String extractClass(String input)
+        {
+                if (input.contains("|"))
+                        input = input.split("\\|")[0];
+
+                if (input.contains(":"))
+                        input = input.split(":")[1];
+
+                return input;
+        }
+
+        @Override
+        public void export(Document document, OutputStream out, FeatureMap options) throws IOException
+        {
+                PrintStream pout = new PrintStream(out);
+                AnnotationSet outputAS = document.getAnnotations(GateConstants.ORIGINAL_MARKUPS_ANNOT_SET_NAME);
+                AnnotationSet mentions = document.getAnnotations().get("Mention");
+
+                //store created annotations so we can remove them later, not relevant in wasp
+                //but quite relevant in gate.app usecase
+                Set<Integer> created = new HashSet<Integer>();
+
+                try
+                {
+                        for (Annotation annotation : mentions)
+                        {
+                                FeatureMap params = Factory.newFeatureMap();
+                                params.put("vocab", "http://dbpedia.org/ontology/");
+                                params.put("resource", annotation.getFeatures().get("inst").toString());
+
+                                if (annotation.getFeatures().get("dbpSpecificClass") != null)
+                                        params.put("typeof",extractClass(annotation.getFeatures().get("dbpSpecificClass").toString()));
+                                else if (annotation.getFeatures().get("class") != null)
+                                        params.put("typeof",annotation.getFeatures().get("class").toString());
+
+                                //params.put("base",extractClass(annotation.getFeatures().get("dbpInterestingClass").toString()));
+
+                                Long start = annotation.getStartNode().getOffset();
+                                Long end = annotation.getEndNode().getOffset();
+
+                                created.add(outputAS.add(start, end, "span", params));
+                        }
+
+                        String result = document.toXml(null, false);
+                        //the gate html document implementation will have added this stuff to the ORIGINAL_MARKUPS
+                        //we do not want these in the RTE or other endpoints
+                        if (options.containsKey("stripOuter") && ((String)options.get("stripOuter")).equals("true"))
+                        {
+                                result = result.replace("<html><head></head><body>","");
+                                result = result.replace("</body></html>","");
+                        }
+                        pout.println(result);
+                }
+                catch(Exception e)
+                {
+                        throw new IOException(e);
+                }
+                finally
+                {
+                        for (Integer id : created)
+                                outputAS.remove(outputAS.get(id));
+                }
+        }
+        }
